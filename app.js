a1478fa4b4f2e59957b06eadbf8bcd5f274f94d2
David Bueno <davidbuenox8@gmail.com>
2021-05-18 16:19:37 +0200

    deployment

--- app.js
+++ app.js
@@ -202,7 +202,13 @@ app.use('/api/users', users);
 const albums = require('./routes/albums');
 app.use('/api/albums', albums);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
 
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
